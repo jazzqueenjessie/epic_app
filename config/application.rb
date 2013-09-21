24e34accf87a661ac419d2917ac1b8e42daba007
jazzqueenjessie <jazzqueenjessie@gmail.com>
2013-09-21 13:07:13 -0700

    precompile fonts and images

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ Bundler.require(:default, Rails.env)
 module EpicApp
   class Application < Rails::Application
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
